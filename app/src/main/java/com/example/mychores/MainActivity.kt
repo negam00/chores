7bd6117df8d23d999b1b0e3c6cadfaf516cfbbf9
negam00 <negam00@github.com>
2020-06-19 22:35:05 +0200

    new version + nav

---
+++ app/src/main/java/com/example/mychores/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.mychores
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(navigation_view, navController)
+
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController,appBarConfiguration)
+    }
+
+}
